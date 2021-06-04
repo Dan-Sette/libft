5a245098bf2b47de81a3314ea2edd8d5939aeb52
Dan-Sette <daniel.sette@hotmail>
2021-06-04 15:12:24 -0300

    Fix the ft_strdup function

--- to_git/42/ft_strdup.c
+++ to_git/42/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dalves-s <dalves-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 12:11:29 by dalves-s          #+#    #+#             */
-/*   Updated: 2021/06/03 12:22:59 by dalves-s         ###   ########.fr       */
+/*   Updated: 2021/06/04 15:08:59 by dalves-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@ char	*ft_strdup(const char *src)
 {
 	char	*dst;
 	size_t	len;
+	size_t	i;
 
-	len = ft_strlen(src);
-	dst = (char *) malloc((len + 1) * sizeof(char));
-	ft_memcpy(dst, src, len);
+	i = 0;
+	len = ft_strlen(src) + 1;
+	dst = (char *) malloc((len) * sizeof(char));
 	if (dst == NULL)
 		return (NULL);
+	while (i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
 	return (dst);
 }
